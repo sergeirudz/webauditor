68e8bd8d3d273f4b4b3ef24157da99a9c380c820
Elmo Egers <uplaymedia@gmail.com>
2023-10-04 18:03:55 +0300

    Audit config additions

--- crawlee/routes.ts
+++ crawlee/routes.ts
@@ -1,5 +1,6 @@
 import { createPlaywrightRouter } from 'crawlee';
 import { playAudit } from 'playwright-lighthouse'
+import lighthouseDesktopConfig from 'lighthouse/core/config/lr-desktop-config'
 
 export const router = createPlaywrightRouter();
 
@@ -15,17 +16,31 @@ router.addHandler('detail', async ({ request, page, log, pushData }) => {
 	const title = await page.title();
 	log.info(`${title}`, { url: request.loadedUrl });
 
-	await playAudit({
-		page,
-		thresholds: {
-			performance: 50,
-			accessibility: 50,
-			'best-practices': 50,
-			seo: 50,
-			pwa: 50,
-		},
-		port: 9222
-	})
+	try {
+		await playAudit({
+			page,
+			config: lighthouseDesktopConfig,
+			thresholds: {
+				performance: 100,
+				accessibility: 100,
+				'best-practices': 100,
+				seo: 100,
+				pwa: 100,
+			},
+			reports: {
+				formats: {
+					json: true, //defaults to false
+					html: true, //defaults to false
+					csv: false
+				}
+				// name: `name-of-the-report`, //defaults to `lighthouse-${new Date().getTime()}`
+				// directory: `path/to/directory`, //defaults to `${process.cwd()}/lighthouse`
+			},
+			port: 9222
+		})
+	} catch (e) {
+		console.error('Caught error', e)
+	}
 
 	await pushData({
 		url: request.loadedUrl,
