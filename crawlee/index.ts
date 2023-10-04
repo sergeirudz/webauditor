967ec0d2fab0ec5493b550575387d87fd72cdc8c
Elmo Egers <uplaymedia@gmail.com>
2023-10-04 23:27:00 +0300

    Update crawling and auditing logic

--- crawlee/index.ts
+++ crawlee/index.ts
@@ -1,17 +1,77 @@
-// For more information, see https://crawlee.dev/
-import { PlaywrightCrawler, ProxyConfiguration } from 'crawlee';
+import { PlaywrightCrawler } from 'crawlee'
+import playwright, {Page} from 'playwright'
+import fs from 'fs';
+import path from 'path';
+import { router } from './routes'
+import {playAudit} from "playwright-lighthouse";
+import lighthouseDesktopConfig from "lighthouse/core/config/lr-desktop-config";
 
-import { router } from './routes';
+const STORAGE_DIR = './storage/datasets/default'
 
-const startUrls = ['https://abipolitseinik.ee'];
+interface FileObject {
+	title: string
+	url: string
+}
+
+const startUrls = ['https://abipolitseinik.ee']
 
 const crawler = new PlaywrightCrawler({
-	launchContext: {
-		launchOptions: {
-			args: ['--remote-debugging-port=9222']
-		}
-	},
-	requestHandler: router,
-});
+	requestHandler: router
+})
 
-await crawler.run(startUrls);
+// await crawler.run(startUrls)
+
+fs.readdir(STORAGE_DIR, async (err, files) => {
+	if (err) {
+		console.error(`Error reading directory: ${err}`)
+		return
+	}
+
+	for (const file of files) {
+		const filePath = path.join(STORAGE_DIR, file)
+
+		const data = fs.readFileSync(filePath, 'utf8')
+
+		const fileObject: FileObject = JSON.parse(data)
+		console.log(`Title: ${fileObject.title}, Url: ${fileObject.url}`)
+
+		const browser = await playwright['chromium'].launch({
+			args: ['--remote-debugging-port=9222']
+		})
+		const page = await browser.newPage()
+		await page.goto(fileObject.url)
+
+		const report = await getReport(page)
+		console.log(report.audits.metrics)
+
+		await browser.close()
+	}
+})
+
+async function getReport(page: Page) {
+	try {
+		const { report } = await playAudit({
+			page,
+			config: lighthouseDesktopConfig,
+			thresholds: {
+				performance: 100,
+				accessibility: 100,
+				'best-practices': 100,
+				seo: 100,
+				pwa: 100,
+			},
+			reports: {
+				formats: {
+					json: true,
+					html: true,
+					csv: false
+				}
+			},
+			ignoreError: true,
+			port: 9222
+		})
+		return JSON.parse(report)
+	} catch (e) {
+		console.log((e as Error).message)
+	}
+}
--- crawlee/routes.ts
+++ crawlee/routes.ts
@@ -1,6 +1,5 @@
 import { createPlaywrightRouter } from 'crawlee';
-import { playAudit } from 'playwright-lighthouse'
-import lighthouseDesktopConfig from 'lighthouse/core/config/lr-desktop-config'
+import { Page } from 'playwright'
 
 export const router = createPlaywrightRouter();
 
@@ -13,37 +12,53 @@ router.addDefaultHandler(async ({ enqueueLinks, log }) => {
 })
 
 router.addHandler('detail', async ({ request, page, log, pushData }) => {
+	const url = request.loadedUrl
 	const title = await page.title();
-	log.info(`${title}`, { url: request.loadedUrl });
-
-	try {
-		await playAudit({
-			page,
-			config: lighthouseDesktopConfig,
-			thresholds: {
-				performance: 100,
-				accessibility: 100,
-				'best-practices': 100,
-				seo: 100,
-				pwa: 100,
-			},
-			reports: {
-				formats: {
-					json: true, //defaults to false
-					html: true, //defaults to false
-					csv: false
-				}
-				// name: `name-of-the-report`, //defaults to `lighthouse-${new Date().getTime()}`
-				// directory: `path/to/directory`, //defaults to `${process.cwd()}/lighthouse`
-			},
-			port: 9222
-		})
-	} catch (e) {
-		console.error('Caught error', e)
-	}
+	log.info(`${title}`, { url });
 
 	await pushData({
 		url: request.loadedUrl,
 		title
 	})
 })
+
+async function getScripts(page: Page, url: string | undefined) {
+	const data = []
+	const scripts = await page.locator('script');
+	const count = await scripts.count();
+	const origin = new URL(url ?? '').origin
+
+	for (let i = 0; i < count; i++) {
+		const script = await scripts.nth(i)
+		let src = await script.getAttribute('src')
+
+		if (src) {
+			if (src.startsWith('//')) {
+				src = origin + src.replace('//', '/')
+			} else if (!src.startsWith('/')) {
+				src = origin + '/' + src
+			} else if (src.startsWith('/')) {
+				src = origin + src
+			}
+
+			const fetched = await fetch(src).then((response) => response.text());
+			data.push({ src, data: fetched })
+		} else {
+			const inline = await script.textContent()
+			// TODO handle inline script - generate hash for filename
+		}
+	}
+
+	// return data
+}
+
+// 	const scripts = await page.locator('script').(elements) => {
+// 		return elements.map((element) => {
+// 			if (element.src) {
+// 				return fetch(element.src).then((response) => response.text());
+// 			} else {
+// 				return element.textContent;
+// 			}
+// 		})
+// 	})
+// }
